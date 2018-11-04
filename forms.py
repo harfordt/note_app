412e3f813d8390e46bde3e1c90a05e3eaf8f35e7
harfordt <tim.harford@wgc.school.nz>
2018-11-05 11:04:30 +1300

    Add category data query and select field

--- forms.py
+++ forms.py
@@ -8,7 +8,6 @@ from sqlite3 import Error
 
 
 class NewNoteForm(Form):
-
     """create a connection to the sqlite db"""
     try:
         con = sqlite3.connect("notes.db")
@@ -25,9 +24,14 @@ class NewNoteForm(Form):
     curr.execute(sql)
     statii = curr.fetchall()
 
+    sql = "SELECT id,name FROM category"
+    curr.execute(sql)
+    categories = curr.fetchall()
+
     con.close()
 
     title = StringField("Title: ", [validators.InputRequired("Please enter a title.")])
+    category = SelectField("Category", choices=categories)
     priority = SelectField("Priority", choices=priorities)
     status = SelectField("Status", choices=statii)
     submit = SubmitField("Add")
